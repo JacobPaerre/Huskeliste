d11ba40b906cbf1857f4ecca53bac62f945cfc5e
Havkost <August.kuhn@gmail.com>
2020-04-15 23:11:50 +0200

    Change tkinter code to fit standard naming conventions

--- ui.py
+++ ui.py
@@ -1,8 +1,14 @@
 # hehe frontend fil
 
 import tkinter as tk
+from tkinter import ttk
 
-top = tk.Tk()
-canvas = tk.Canvas(top, height=700, width=700, bg="#ff00ff")
-top.mainloop()
+root = tk.Tk()
 
+#canvas = tk.Canvas(root, height=700, width=700, bg="#FF00FF")
+#canvas.pack()
+
+frame = tk.Frame(root, bg="#FFFFFF")
+frame.place(relheight=0.8, relwidth=0.8, relx=0.1, rely=0.1)
+
+root.mainloop()
