f30ef9c093148f19a866cec2b8834b3644ac225b
drabxd <43722194+drabxd@users.noreply.github.com>
2020-04-16 15:13:50 +0200

    Update test.py

--- test.py
+++ test.py
@@ -3,18 +3,22 @@ from tkinter import *
 import os
 
 root = tk.Tk()
+root.resizable(0,0)
 
-# Baggrund
+# Canvas
 canvas = tk.Canvas(root, height=800, width=800, bg="white")
 canvas.pack()
 
 # Venstre del af UI
 frame = tk.Frame(root, bg="#1D4147")
-frame.place(relwidth=0.225, relheight=1)
+frame.place(relwidth=.215, relheight=1)
+
+titel = tk.Label(frame, text="Huskeliste", bg="#1D4147",fg="black", font=("Ubuntu, 24"),)
+titel.pack()
 
 # Split mellem højre og venstre
 frame = tk.Frame(root, bg="#227373")
-frame.place(x=170, relwidth=0.03, relheight=1)
+frame.place(x=170, relwidth=0.1, relheight=1)
 
 # Højre del af UI (toppen)
 frame = tk.Frame(root, bg="#208C81")
