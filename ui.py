07b0512e73204b4185b8e3aa7f6f8646854ad4a8
drabxd <43722194+drabxd@users.noreply.github.com>
2020-04-16 14:48:25 +0200

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,14 +1,29 @@
-# hehe frontend fil
-
 import tkinter as tk
-from tkinter import ttk
+from tkinter import *
+import os
 
 root = tk.Tk()
 
-#canvas = tk.Canvas(root, height=700, width=700, bg="#FF00FF")
-#canvas.pack()
+# Baggrund
+canvas = tk.Canvas(root, height=800, width=800, bg="white")
+canvas.pack()
 
-frame = tk.Frame(root, bg="#FFFFFF")
-frame.place(relheight=0.8, relwidth=0.8, relx=0.1, rely=0.1)
+# Venstre del af UI
+frame = tk.Frame(root, bg="#1D4147")
+frame.place(relwidth=0.225, relheight=1)
 
+# Split mellem højre og venstre
+frame = tk.Frame(root, bg="#227373")
+frame.place(x=170, relwidth=0.03, relheight=1)
+
+# Højre del af UI (toppen)
+frame = tk.Frame(root, bg="#208C81")
+frame.place(x=194, relwidth=1, relheight=0.08)
+
+#Højre del af UI (bunden)
+frame = tk.Frame(root, bg="#404040")
+frame.place(x=194, y=64, relwidth=1, relheight=1)
+
+
+# Starter UI'en
 root.mainloop()
