b4ebd42d8e5fdd445abb55ca06b36a5876e79d12
Havkost <August.kuhn@gmail.com>
2020-04-24 23:56:41 +0200

    Create fully functional borderless window

--- ui.py
+++ ui.py
@@ -3,17 +3,25 @@ import os
 
 # STYLES TIL GUI
 
+# Borderless window movement code from Bryan Oakley on stackoverflow: https://stackoverflow.com/a/4055612/13319955
 
 class App():
     def __init__(self):
         self.root = tk.Tk()
         self.root.geometry("576x324")
-        self.root.overrideredirect(0)
+        self.root.overrideredirect(1)
         self.windowed = True
 
         # Bar til windowed mode og muligvis exit
         self.managementbar = tk.Frame(self.root, height=25, bg="#75A08D")
         self.managementbar.pack(fill=tk.X, side=tk.TOP)
+
+        self.managementbar.bind("<ButtonPress-1>", self.StartMove)
+        self.managementbar.bind("<ButtonRelease-1>", self.StopMove)
+        self.managementbar.bind("<B1-Motion>", self.OnMotion)
+
+        self.quitbutton = tk.Button(self.managementbar, text="Exit", command=self.root.destroy)
+        self.quitbutton.pack(side=tk.RIGHT)
 
         self.windowbutton = tk.Button(self.managementbar, text="Toggle window", command=self.toggleWindow)
         self.windowbutton.pack(side=tk.RIGHT)
@@ -28,6 +36,9 @@ class App():
         self.addbutton = tk.Button(self.sidebar, text="+", command=self.addList, font=("Ubuntu 16 bold"), bg="#1D4147", bd=0)
         self.addbutton.pack()
 
+        self.listnavcontainer = tk.Frame(self.sidebar)
+        self.listnavcontainer.pack()
+
         # Split mellem højre og venstre
         self.sidebarsplit = tk.Frame(self.root, width=20,bg="#227373")
         self.sidebarsplit.pack(fill=tk.Y, side=tk.LEFT)
@@ -41,20 +52,33 @@ class App():
         self.listdesk.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
 
     def toggleWindow(self):
-            if self.windowed:
-                self.windowed = False
-                self.root.geometry(str(self.root.winfo_screenwidth())+"x"+str(self.root.winfo_screenheight())+"+0+0")
-                self.root.overrideredirect(1)
-            else:
-                self.windowed = True
-                self.root.minsize(576, 324)
-                self.root.geometry("576x324")
-                self.root.overrideredirect(0)
+        if self.windowed:
+            self.windowed = False
+            self.root.geometry(str(self.root.winfo_screenwidth())+"x"+str(self.root.winfo_screenheight())+"+0+0")
+        else:
+            self.windowed = True
+            self.root.minsize(576, 324)
+            self.root.geometry("576x324")
 
     def addList(self):
-        self.liste = tk.Button(self.sidebar, text="123", font=("Ubuntu", 12), fg="white", bg="#1D4147", bd=0)
+        self.liste = tk.Button(self.listnavcontainer, text="123", font=("Ubuntu", 12), fg="white", bg="#1D4147", bd=0)
         self.liste.pack()
 
+    def StartMove(self, event):
+        self.x = event.x
+        self.y = event.y
+
+    def StopMove(self, event):
+        self.x = None
+        self.y = None
+
+    def OnMotion(self, event):
+        if self.windowed:
+            deltax = event.x - self.x
+            deltay = event.y - self.y
+            x = self.root.winfo_x() + deltax
+            y = self.root.winfo_y() + deltay
+            self.root.geometry("+%s+%s" % (x, y))
 
 app = App()
 # Starter UI'en
